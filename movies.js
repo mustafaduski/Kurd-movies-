511707c79af8cf245f745200d41f0e7e8e915958
mustafaduski <1ksport76@gmail.com>
2026-02-08 20:43:07 +0300

    Update and rename movies.json to movies.js

---
+++ movies.js
@@ -0,0 +1,6 @@
+const movies = [
+    { id: 1, title: "Inception", cat: "Sci-Fi", poster: "https://image.tmdb.org/t/p/w500/edv5CZvj0Yj97o9IBnoHQokzSmy.jpg", link: "...", desc: "..." },
+    { id: 2, title: "Interstellar", cat: "Sci-Fi", poster: "https://image.tmdb.org/t/p/w500/gEU2QniE6EwfVnz6S3pAUjCBEUu.jpg", link: "...", desc: "..." },
+    // لێرە تو دشێی هەتا ١٠٠٠ فیلمـان زێدە بکەی ب بێ کێشە
+];
+export default movies;
